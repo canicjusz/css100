71e6b47cd25889b80c6cba46c7591590e93b5df6
Jan <janek.bananek371@gmail.com>
2019-10-14 12:54:07 +0200

    1st challange (fixed animation at load)

--- 1/script.js
+++ 1/script.js
@@ -1,10 +1,16 @@
 var hams = document.querySelector('.ham-container').children
 var hamburger = document.querySelector('.ham-container')
-
+var j = 0
 hamburger.addEventListener('click', _=>{
     for(let i = 0; i < hams.length; i++){
         hams[i].parentNode.replaceChild(hams[i].cloneNode(), hams[i])
-        hams[i].classList.toggle(`off${i+1}`)
-        hams[i].classList.toggle(`on${i+1}`)
+        if(j%2==0){
+            hams[i].classList.add(`off${i+1}`)
+            hams[i].classList.remove(`on${i+1}`)
+        }else{
+            hams[i].classList.remove(`off${i+1}`)
+            hams[i].classList.add(`on${i+1}`)
+        }
     }
+    j++
 })
